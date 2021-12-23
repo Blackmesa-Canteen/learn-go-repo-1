ba7e71af502ed6f7076eeb0bdaa75469a7aeb780
996Worker <69796042+Blackmesa-Canteen@users.noreply.github.com>
2021-12-23 11:21:04 +0800

    一些一本的路由和模板

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func main() {
 
 	// 默认路由
 	r := gin.Default()
 
+	// 模板路径
+	r.LoadHTMLGlob("templates/*")
+
 	// route配置
 	r.GET("/", func(context *gin.Context) {
-		context.String(200, "你好值: %v", "好的")
+		context.String(http.StatusOK, "你好值: %v", "好的")
+	})
+
+	r.GET("/news", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "newa.html", gin.H{
+			"title": "我是你爸爸",
+		})
+	})
+
+	r.GET("/json", func(context *gin.Context) {
+		context.JSON(http.StatusOK, map[string]interface{}{
+			"success": true,
+			"msg":     "hello, yes sir",
+		})
+	})
+
+	r.GET("/json2", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"msg":     "json2",
+		})
 	})
 
 	// web服务
-	r.Run()
+	err := r.Run(":8000")
+	if err != nil {
+		return
+	}
 }
